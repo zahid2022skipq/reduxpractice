e2aa4c0b0de18851da8402840bb0f90595a56883
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-15 12:30:02 +0500

    Project setup

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h2>Zahid Account</h2>
+      </div>
+
+      <div>
+        <h4>Balance: 0$</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
